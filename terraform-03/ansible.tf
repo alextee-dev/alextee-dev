1d6daf5a68bab67b9b64d008aa0a243bd74e7b90
Hellsong <hellsong@pes.com>
2024-12-30 19:33:06 +0700

    Задание 6

--- terraform-03/ansible.tf
+++ terraform-03/ansible.tf
@@ -6,3 +6,30 @@ resource "local_file" "hosts_templatefile" {
 
   filename = "${abspath(path.module)}/hosts.ini"
 }
+
+resource "null_resource" "web_hosts_provision" {
+  depends_on = [yandex_compute_instance.platform, yandex_compute_instance.platform2, yandex_compute_instance.platform3]
+
+  #Добавление ПРИВАТНОГО ssh ключа в ssh-agent
+  provisioner "local-exec" {
+    command    = "> ~/.ssh/known_hosts &&eval $(ssh-agent) && cat ~/.ssh/ycservice | ssh-add -"
+    on_failure = continue
+  }
+
+  provisioner "local-exec" {
+     command = "sleep 120"
+   }
+
+  #Запуск ansible-playbook
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ${abspath(path.module)}/hosts.ini ${abspath(path.module)}/test.yml"
+    on_failure  = continue
+    environment = { ANSIBLE_HOST_KEY_CHECKING = "False" }
+  }
+  triggers = {
+    always_run      = "${timestamp()}"
+    always_run_uuid = "${uuid()}"
+
+  }
+
+}
