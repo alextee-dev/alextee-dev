a2fcd02fd6c03ee0fd1b2d5887b86538cc665ac3
Hellsong <hellsong@pes.com>
2025-01-17 18:04:22 +0700

    Задание 5

--- Домашнее задание к занятию «Продвинутые методы работы с Terraform»/main.tf
+++ Домашнее задание к занятию «Продвинутые методы работы с Terraform»/main.tf
@@ -1,52 +1,53 @@
-module "develop" {
-  source = "./vpc"
-  name = "${var.vpc_name}"
-  zone = var.zone1
-  v4_cidr_blocks = var.cidr1
+# Модуль создания сети prod
+module "vpc_prod" {
+  source       = "./vpc"
+  env_name     = var.prod_name
+  name = var.prod_name
+  subnets = var.vpc_prod
 }
 
-module "marketing-vm" {
-  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
-  env_name       = "develop" 
-  network_id     = module.develop.yandex_vpc_network.id
-  subnet_zones   = [var.zone1]
-  subnet_ids     = [module.develop.yandex_vpc_subnet.id]
-  instance_name  = var.vm_marketing_name
-  instance_count = 2
-  image_family   = var.vm_os_family
-  public_ip      = true
-
-  labels = { 
-    project = "marketing"
-     }
-
-  metadata = {
-    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
-    serial-port-enable = 1
-  }
-
+# Модуль создания сети dev
+module "vpc_dev" {
+  source       = "./vpc"
+  env_name     = var.dev_name
+  name = var.dev_name
+  subnets = var.vpc_dev
 }
 
-module "analytics-vm" {
-  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
-  env_name       = "stage"
-  network_id     = module.develop.yandex_vpc_network.id
-  subnet_zones   = [var.zone1]
-  subnet_ids     = [module.develop.yandex_vpc_subnet.id]
-  instance_name  = var.vm_analytics_name
-  instance_count = 1
-  image_family   = var.vm_os_family
-  public_ip      = true
+# Модуль создания кластера managed MySQL
+module "mysql" {
+  source = "./mysql"
+  name = var.cluster1_name
+  network_id = module.vpc_prod.yandex_vpc_network.id
+  subnet_id = module.vpc_prod.yandex_vpc_subnet.ru-central1-a.id
+  HA = true
+  
+}
 
-  labels = { 
-    project = "analytics"
-     }
+# Модуль создания БД и пользователя в кластере managed
+module "mysql_db" {
+  source = "./mysql_db"
+  cluster_id = module.mysql.yandex_mdb_mysql_cluster.id
+  db_name = var.db_managed_name
+  db_user = var.db_managed_user
+}
 
-  metadata = {
-    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
-    serial-port-enable = 1
-  }
+# Модуль создания кластера example MySQL
+module "mysql_example" {
+  source = "./mysql"
+  name = var.cluster2_name
+  network_id = module.vpc_prod.yandex_vpc_network.id
+  subnet_id = module.vpc_prod.yandex_vpc_subnet.ru-central1-a.id
+  HA = false
+  
+}
 
+# Модуль создания БД и пользователя в кластере example
+module "mysql_db_example" {
+  source = "./mysql_db"
+  cluster_id = module.mysql_example.yandex_mdb_mysql_cluster.id
+  db_name = var.db_example_name
+  db_user = var.db_example_user
 }
 
 data template_file "cloudinit" {
@@ -56,3 +57,4 @@ data template_file "cloudinit" {
     ssh_public_key     = file(var.vms_ssh_root_key)
   }
 }
+
--- Домашнее задание к занятию «Продвинутые методы работы с Terraform»/variables.tf
+++ Домашнее задание к занятию «Продвинутые методы работы с Terraform»/variables.tf
@@ -64,4 +64,78 @@ variable "vm_os_family" {
   description = "OS Family"
 }
 
+variable "vpc_prod" {
+  description = "List of subnets with zones and CIDR blocks"
+  type = list(object({
+    zone = string
+    cidr = list(string)
+  }))
+  default = [
+    {
+      zone = "ru-central1-a"
+      cidr = ["10.0.1.0/24"]
+    },
+    {
+      zone = "ru-central1-b"
+      cidr = ["10.0.2.0/24"]
+    },
+    {
+      zone = "ru-central1-d"
+      cidr = ["10.0.3.0/24"]
+    }
+  ]
+}
 
+variable "vpc_dev" {
+  description = "List of subnets with zones and CIDR blocks"
+  type = list(object({
+    zone = string
+    cidr = list(string)
+  }))
+  default = [
+    {
+      zone = "ru-central1-a"
+      cidr = ["10.0.1.0/24"]
+    }
+  ]
+}
+
+variable "prod_name" {
+  type        = string
+  default     = "prod"
+}
+
+variable "dev_name" {
+  type        = string
+  default     = "dev"
+}
+
+variable "cluster1_name" {
+  type = string
+  default = "managed"
+}
+
+variable "cluster2_name" {
+  type = string
+  default = "example"
+}
+
+variable "db_managed_name" {
+  type = string
+  default = "QWERTYdb"
+}
+
+variable "db_managed_user" {
+  type = string
+  default = "testuser"
+}
+
+variable "db_example_name" {
+  type = string
+  default = "test"
+}
+
+variable "db_example_user" {
+  type = string
+  default = "app"
+}
