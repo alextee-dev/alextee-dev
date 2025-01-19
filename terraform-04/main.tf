35d61d114440e9e0360350b4323df7ab032958f6
Hellsong <hellsong@pes.com>
2025-01-19 17:28:20 +0700

    Final без 8 задания

--- terraform-04/main.tf
+++ terraform-04/main.tf
@@ -50,6 +50,51 @@ module "mysql_db_example" {
   db_user = var.db_example_user
 }
 
+
+module "marketing-vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = var.prod_name
+  network_id     = module.vpc_prod.yandex_vpc_network.id
+  subnet_zones   = [var.zone1,var.zone2]
+  subnet_ids     = [module.vpc_prod.yandex_vpc_subnet.ru-central1-a.id,module.vpc_prod.yandex_vpc_subnet.ru-central1-b.id]
+  instance_name  = var.vm_marketing_name
+  instance_count = 2
+  image_family   = var.vm_os_family
+  public_ip      = true
+
+  labels = {
+    project = "marketing"
+     }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+    serial-port-enable = 1
+  }
+
+}
+
+module "analytics-vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "stage"
+  network_id     = module.vpc_dev.yandex_vpc_network.id
+  subnet_zones   = [var.zone1]
+  subnet_ids     = [module.vpc_dev.yandex_vpc_subnet.ru-central1-a.id]
+  instance_name  = var.vm_analytics_name
+  instance_count = 1
+  image_family   = var.vm_os_family
+  public_ip      = true
+
+  labels = {
+    project = "analytics"
+     }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+    serial-port-enable = 1
+  }
+
+}
+
 data template_file "cloudinit" {
   template = file("./cloud-init.yml")
 
@@ -57,4 +102,3 @@ data template_file "cloudinit" {
     ssh_public_key     = file(var.vms_ssh_root_key)
   }
 }
-
